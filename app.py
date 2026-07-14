94fd42a73516e6d1f5ab58af7e0e8afd3c0edcd9
ay7307867729-stack <ay7307867729@gmail.com>
2026-07-14 13:31:40 -0700

    fix indentation

--- app.py
+++ app.py
@@ -64,11 +64,10 @@ messages=[
         })
 
     except Exception as e:
-    print("ERROR:", e)
-    return jsonify({
-        "error": str(e)
-    }), 500
-
+        print("ERROR:", e)
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
